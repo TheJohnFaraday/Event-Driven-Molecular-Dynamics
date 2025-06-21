4a0c63fb827ef7c886e3ac2bd426ef074f81085f
Martín E. Zahnd <mzahnd@itba.edu.ar>
2025-06-20 22:58:24 -0300

    OK 1

--- src/main/kotlin/Simulation.kt
+++ src/main/kotlin/Simulation.kt
@@ -1,5 +1,6 @@
 package ar.edu.itba.ss
 
+import java.io.BufferedWriter
 import java.util.*
 
 fun runSimulation(
@@ -12,43 +13,43 @@ fun runSimulation(
     )
 
     val motor = ParticleMotor(particles, settings.containerRadius)
-    val pq = PriorityQueue<CollisionData>()
+    val priorityQueue = PriorityQueue<CollisionData>()
 
     var now = 0.0
-    for (p in particles) {
-        pq.addAll(motor.predict(p, now))
-    }
+    particles.forEach { particle -> priorityQueue.addAll(motor.predict(particle, now)) }
 
     settings.outputFile.bufferedWriter().use { out ->
-        dump(out, now, particles)
+        write(out, now, particles)
         while (now < settings.finalTime) {
-            val c = pq.poll() ?: break
-            if (!c.isValid()) continue
-            val dt = c.time - now
+            val collisionData = priorityQueue.poll() ?: break
+            if (!collisionData.isValid()) continue
+            val dt = collisionData.time - now
 
-            for (p in particles) {
-                p.move(dt)
-            }
+            particles.forEach { it.move(dt) }
 
-            now = c.time
-            motor.resolve(c)
+            now = collisionData.time
+            motor.resolve(collisionData)
 
-            pq.addAll(motor.predict(c.a, now))
-            c.b?.let { pq.addAll(motor.predict(it, now)) }
+            priorityQueue.addAll(motor.predict(collisionData.a, now))
+            collisionData.b?.let { priorityQueue.addAll(motor.predict(it, now)) }
 
-            dump(out, now, particles)
+            write(out, now, particles)
         }
     }
 
     println("Simulation finished. States written to ${settings.outputFile}")
 }
 
-private fun dump(out: Appendable, t: Double, ps: List<Particle>) {
-    out.appendLine(String.format(Locale.US, "%.6f", t))
-    for (p in ps) {
-        out.appendLine(
-            String.format(Locale.US, "%d %.6f %.6f %.6f %.6f", p.id, p.x, p.y, p.vx, p.vy)
-        )
-    }
+private fun write(out: BufferedWriter, time: Double, particles: List<Particle>) {
+    out.appendLine("%.6f".format(time))
+    particles.map { p ->
+        listOf(
+            p.id,
+            "%.6f".format(p.x),
+            "%.6f".format(p.y),
+            "%.6f".format(p.vx),
+            "%.6f".format(p.vy),
+        ).joinToString(separator = " ")
+    }.map { out.appendLine(it) }
     out.appendLine("---")
 }
